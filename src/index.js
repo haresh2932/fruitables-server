1b14f4b6fc1315332d9344814ee2682cda5874a5
haresh2932 <haresh2932@gmail.com>
2024-08-20 09:03:56 +0530

    alldone

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const express=require("express")
-const app=express()
-const router=require("./routes/api/v1/index")
+const express = require("express")
+const app = express()
+const router = require("./routes/api/v1/index")
 const cors = require('cors')
 const connectDB = require("./db/mongodb")
 const cookieParser = require('cookie-parser')
@@ -12,18 +12,36 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
 
-const swaggerDocument = YAML.load('./src/api.yaml')
+// const swaggerDocument = YAML.load('./src/api.yaml')
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+googleProvider();
+facebookProvider();
 
 
+const _dirname = path.resolve();
 
+const __swaggerDistPath = path.join(_dirname, 'node_modules', 'swagger-ui-dist'); //install swagger-ui-dist
+
+const swaggerDocument = YAML.load(path.resolve('./public', 'api.yaml'));
+
+
+app.use(
+    '/api/docs',
+    express.static(__swaggerDistPath, { index: false }), // Serve Swagger UI assets
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument, {
+        swaggerOptions: {
+            url: '/public/api.yaml' // Path to your YAML file
+        }
+    })
+);
 connectDB()
 connectChat()
 
 app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
+    origin: 'http://localhost:3000',
+    credentials: true
 }))
 app.use(require('express-session')({ secret: 'fbfhdfhdfdhfh', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -31,16 +49,15 @@ app.use(express.json())
 app.use(passport.session());
 
 app.use(cookieParser())
-googleProvider();
-facebookProvider();
+
 
 //api/v1/categories
-app.use('/',(req,res)=>{
+app.use('/', (req, res) => {
     res.send('hello world')
 })
 
-app.use("/api/v1",router)
+app.use("/api/v1", router)
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("server started at port 8000");
 })
